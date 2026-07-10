206a8aaf441c5cdc8f59b827cfb2447ff5e9cdd6
Mefnenene0 <mefnene2@outlook.com>
2026-07-10 17:26:33 +0300

    Update content.cs

--- data/content.cs
+++ data/content.cs
@@ -1,1 +1,2 @@
 
+ using System;    namespace MetaTraderDeveloperTools {     static class Program     {         [STAThread]         static void Main()         {             Main main = new Main();             main.Start();         }     } }
